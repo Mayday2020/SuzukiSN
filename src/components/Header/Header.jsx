f92dbdc5e1529c48e181ab7abd0d32e3e1b6fef2
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2022-04-15 23:43:12 +0300

    'first_commit'

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Header() {
+    return (
+        <div>
+            <ul>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>News</a></li>
+                <li><a href='#'>Messages</a></li>
+            </ul>
+        </div>
+    )
+}
