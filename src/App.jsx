78a0798a92566c5a8b009de4e35217d735b378b6
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2022-04-18 00:36:27 +0300

    'MUI_profile'

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import './App.css';
 import {Header} from "./components/Header/Header";
-import {Footer} from "./components/Footer/Footer";
+import {Navbar} from "./components/Navbar/Navbar";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Main from "./components/Main/Main";
+import {Typography} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
-  );
+    return (
+        <Container fixed>
+            <Grid container>
+                <Grid item xs={12}>
+                    <Header />
+                </Grid>
+                <Grid item xs={2}>
+                    <Navbar/>
+                </Grid>
+                <Grid item xs={10}>
+                    <Main/>
+                </Grid>
+                <Grid item xs={12} sx={{backgroundColor: 'darkgrey'}}>
+                    <Typography>
+                        This is Footer component
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }
 
 export default App;
